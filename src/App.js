146dd0be727853f1ea2ad711a0119a116ee40228
Janhavi Soni <jsoni@BLR.ALLEGISINDIA.COM>
2023-05-01 17:33:25 +0530

    Project Completed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import NavBar from './component/NavBar';
+import Outer from './component/Outer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar title="Pokemon Search Application" />
+      <Outer/>
+    </div>
+
+  
+
+  );
+}
+
+export default App;
